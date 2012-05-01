3e517acc58506a4117e03ee1d17631b2095de269
John Peck <john@hippo.(none)>
2012-04-30 17:55:55 -0700

    Added the png directory for non-xfig image files

---
+++ marketing/magazine/pngs/termgrab.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Name: termgrab.sh
+#
+# Grabs a terminal window and creates the image file "grab.eps"
+#
+# Usage: termgrab.sh
+# ...then click in the window you want to grab with the crosshairs.
+set -e # bash should exit the script if any statement returns a non-true 
+       #return value
+echo "Use the crosshair to select the window to grab."
+# Options for import:
+# -frame -- Include the window frame with the window grab.
+import -frame capture.png
+# Options for convert:
+# -resample XRESxYRES -- set the image resolution
+# -negate -- Invert each pixel color
+convert -resample 50x50 \
+	-negate \
+	capture.png grab.eps
+echo "Window image saved to grab.eps."
